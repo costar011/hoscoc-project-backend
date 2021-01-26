223528b42c6f77048eaa1e2f56917aa34d81d0ff
YERIM <dnaer456@gmail.com>
2021-01-27 00:25:29 +0900

    DB CONNECTION

---
+++ db/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const connect = () => {
+  if (process.env.NODE_ENV !== `production`) {
+    mongoose.set("debug", true); // debug모드로 실행된다.
+  }
+
+  mongoose.connect(
+    `mongodb://${process.env.DB_ID}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_USER}`,
+    {
+      dbName: process.env.DB_NAME,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      // `` <-- 첫번째 파라미터 연결 주소
+    },
+    (error) => {
+      if (error) {
+        console.log(error);
+        console.log(`❌ CONNECTION FAILED TRY AGAIN`);
+      } else {
+        console.log(`💚 MONGODB CONNECTION SUCCEES`);
+      }
+    }
+  );
+};
+
+mongoose.connection.on(`error`, (error) => {
+  console.log(error);
+  console.log(`❌ CONNECTION FAILED TRY AGAIN`);
+  connect();
+});
+
+mongoose.connection.on(`disconnect`, () => {
+  console.log(`❌ CONNECTION FAILED TRY AGAIN`);
+  connect();
+});
+
+module.exports = connect;
